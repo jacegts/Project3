c3f90d143f465327d236e77ce801d5d81d0bc19c
jacegts <jacegts@gmail.com>
2015-03-31 21:28:58 -0600

    Latest
    Got a lot of stuff working.  Need to work on figuring out why its not
    seeing AuthKey table in SQLite.   Maybe look into making a $postData
    object like before so we aren't constantly referencing globals.
    Research ajax interaction once we get a success/failure notice.  Lookup
    sending back HTTP error codes for the API.

---
+++ src/Views/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+$("#submit").click(function(){
+var name = $("#name").val();
+var password = $("#password").val();
+// Returns successful data submission message when the entered information is stored in database.
+var dataString = 'name1='+ name + '&password1='+ password;
+if(name==''||password=='')
+{
+alert("Please Fill All Fields");
+}
+else
+{
+// AJAX Code To Submit Form.
+$.ajax({
+type: "POST",
+url: "ajaxsubmit.php",
+data: dataString,
+cache: false,
+success: function(result){
+alert(result);
+}
+});
+}
+return false;
+});
+});
